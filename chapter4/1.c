f59337ab40b1a92264ba98a99e0193a987e2bde2
dreamer2018 <zhoupans_mail@163.com>
2015-10-18 21:24:56 +0800

    add

---
+++ chapter4/1.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: ZhouPan / github:dreamer2018 
+	> Mail: zhoupans_mail@163.com
+	> Blog: blog.csdn.net/it_dream_er
+	> Declared: Signed on behalf of not only belongs , but also represent a responsibility!
+	> Created Time: Sun 18 Oct 2015 09:24:34 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+wqferwgb
