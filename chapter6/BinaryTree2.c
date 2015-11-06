b7f436956ab1ff1c347e39706accdd01c75bc839
dreamer2018 <zhoupans_mail@163.com>
2015-11-06 08:47:34 +0800

    add

--- chapter6/BinaryTree2.c
+++ chapter6/BinaryTree2.c
@@ -37,7 +37,7 @@ void Init(StackNode *S)
 //判空
 int IsEmpty(StackNode *S)
 {
-    if(S->top<=0)
+    if(S->top<0)
     {
         return 1;
     }
@@ -52,10 +52,10 @@ int IsFull(StackNode *S)
     }
     return 0;
 }
-//出栈
+//入栈
 int Push(StackNode *S,ElemType e)
 {
-    if(S->top>=MAXSIZE-1)
+    if(IsFull(S))
     {
         printf("Stack is full!\n");
         return 0;
@@ -65,10 +65,10 @@ int Push(StackNode *S,ElemType e)
     return 1;
 }
 
-//入栈
+//出栈
 int Pop(StackNode *S,ElemType *e)
 {
-    if(S->top<0)
+    if(IsEmpty(S))
     {
         printf("Stack is empty!\n");
         return 0;
@@ -76,6 +76,33 @@ int Pop(StackNode *S,ElemType *e)
     *e=S->elem[S->top];
     S->top--;
 }
+//先序创建二叉树
+BinaryTree *PreCreated()
+{
+    char ch;
+    ch=getchar();
+    if(ch=='^')
+    {
+        return NULL;
+    }
+    BinaryTree *B;
+    B=(BinaryTree *)malloc(sizeof(BinaryTree));
+    B->data=ch;
+    B->LChild=PreCreated();
+    B->RChild=PreCreated();
+    return B;
+}
+//先序遍历二叉树
+void PreOrder(BinaryTree *B)
+{
+    if(B)
+    {
+        printf("%c\t",B->data);
+        PreOrder(B->LChild);
+        PreOrder(B->RChild);
+    }
+}
+/*
 //先序创建二叉树
 BinaryTree *PreCreated()
 {
@@ -90,18 +117,53 @@ BinaryTree *PreCreated()
         ch=getchar();
         if(ch=='#')
         {
-
         }
-    }while(S->top==0 && B->LChild)
+    }while(S->top==0 && B->LChild);
 
-    while(S)
 }
+*/
 //中序创建二叉树
 //后序创建二叉树
-
+//输出栈中所有元素
+void PrintStack(StackNode *S)
+{
+    int i;
+    for(i=0;i<=S->top;i++)
+    {
+       printf("%c",S->elem[i]);
+    }
+    printf("\n");
+}
 //先序打印出从根到叶子结点的路径
+void PrePrint(BinaryTree *root,StackNode *S)
+{
+    if(root)
+    {
+        ElemType e;
+        Push(S,root->data);
+        if(root->LChild==NULL && root->RChild==NULL)
+        {
+            Pop(S,&e);
+            printf("\n%c:",e);
+            PrintStack(S);
+        }
+        else
+        {
+            PrePrint(root->LChild,S);
+            PrePrint(root->RChild,S);
+            Pop(S,&e);
+        }
+    }
+}
 
 int main()
 {
-    
+    StackNode *S;
+    BinaryTree *B;
+    S=(StackNode *)malloc(sizeof(StackNode));
+    Init(S);
+    B=PreCreated();
+    PreOrder(B);
+    PrePrint(B,S);
+    printf("\n");
 }
