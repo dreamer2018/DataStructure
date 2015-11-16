21c9d2e9c81a15acf41012be2cca500319e79742
dream2018 <zhoupans_mail@163.com>
2015-11-16 15:15:11 +0800

    debug

--- chapter6/Forest.c
+++ chapter6/Forest.c
@@ -65,10 +65,40 @@ void RootFinal(ForestNode *f)
         RootFinal(f->NextSibling);
     }
 }
-
+//求叶子节点个数
+int leaf=0;
+void Count_Leaf1(ForestNode *f)
+{
+    if(f)
+    {
+        if(f->FirstChild==NULL)
+        {
+            leaf++;
+        }
+        Count_Leaf1(f->FirstChild);
+        Count_Leaf1(f->NextSibling);
+    }
+}
+//求叶子节点个数方法2
+int Count_Leaf2(ForestNode *f)
+{
+    int h1=0,h2=0,h=0;
+    if(f)
+    {
+        if(f->FirstChild==NULL)
+        {
+            h++;
+        }
+        h1=Count_Leaf2(f->FirstChild);
+        h2=Count_Leaf2(f->NextSibling);
+        return h1+h2+h;
+    }
+    return 0;
+}
 //计算森林的高度
 int high=0;
-void Count_High(ForestNode *f,int h)
+
+void Count_High1(ForestNode *f,int h)
 {
     if(f)
     {
@@ -76,11 +106,22 @@ void Count_High(ForestNode *f,int h)
         {
             high=h;
         }
-        Count_High(f->FirstChild,h+1);
-        Count_High(f->NextSibling,h);
+        Count_High1(f->FirstChild,h+1);
+        Count_High1(f->NextSibling,h);
     }
 }
 //计算森林的高度方法二
+int Count_High2(ForestNode *f)
+{
+    if(f)
+    {
+        int n=Count_High2(f->FirstChild);
+        int m=Count_High2(f->NextSibling);
+        return (m>n ? m:n+1);
+    }
+    return 0;
+}
+
 int main()
 {
     ForestNode *f;
@@ -89,6 +130,8 @@ int main()
     printf("\n");
     RootFinal(f);
     printf("\n");
-    Count_High(f,1);
+    high=Count_High2(f);
     printf("%d\t\n",high);
+    leaf=Count_Leaf2(f);
+    printf("%d\t\n",leaf);
 }
