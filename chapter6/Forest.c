36744cd81a81c692f6be560dceab5176cd3111c3
dream2018 <zhoupans_mail@163.com>
2015-11-16 14:46:39 +0800

    debug

--- chapter6/Forest.c
+++ chapter6/Forest.c
@@ -11,49 +11,84 @@
 #include<malloc.h>
 
 //定义森林的数据类型
-#define char DATATYPE
+#define DATATYPE char
 
 //定义森林的数据结构
 typedef struct forest
 {
     DATATYPE data;
-    struct forest FirstChild;
-    struct forest NextSibling;
+    struct forest *FirstChild;
+    struct forest *NextSibling;
 }ForestNode;
 
 //先序创建森林
-void Created(ForestNode *f)
+void Created(ForestNode **f)
 {
     DATATYPE ch;
     ch=getchar();
     if(ch=='^')
     {
-        f=NULL;
+        *f=NULL;
     }
     else
     {
-        f->data=ch;
-        Created(f->FirstChild);
-        Created(f->NextSibling);
+        *f=(ForestNode *)malloc(sizeof(ForestNode));
+        (*f)->data=ch;
+        Created(&((*f)->FirstChild));
+        Created(&((*f)->NextSibling));
     }
 }
 
-//先根便利二叉树
+//先根遍历二叉树
 void RootFirst(ForestNode *f)
 {
     ForestNode *t;
     t=f;
     if(t!=NULL)
     {
-        printf("%c\t",f->data);
+        printf("%c\t",t->data);
+        t=t->FirstChild;
         while(t)
         {
-            t=t->FirstChild;
-
+            RootFirst(t);
+            t=t->NextSibling;
         }
     }
 }
+//后根遍历
+void RootFinal(ForestNode *f)
+{
+    if(f)
+    {
+        RootFinal(f->FirstChild);
+        printf("%c\t",f->data);
+        RootFinal(f->NextSibling);
+    }
+}
+
+//计算森林的高度
+int high=0;
+void Count_High(ForestNode *f,int h)
+{
+    if(f)
+    {
+        if(high<h)
+        {
+            high=h;
+        }
+        Count_High(f->FirstChild,h+1);
+        Count_High(f->NextSibling,h);
+    }
+}
+//计算森林的高度方法二
 int main()
 {
-    
+    ForestNode *f;
+    Created(&f);
+    RootFirst(f);
+    printf("\n");
+    RootFinal(f);
+    printf("\n");
+    Count_High(f,1);
+    printf("%d\t\n",high);
 }
