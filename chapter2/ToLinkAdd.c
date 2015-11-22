4a566a5e6f0d701128750e0b9ddb5af81cc7b93c
dream2018 <zhoupans_mail@163.com>
2015-11-22 16:32:02 +0800

    add note

--- chapter2/ToLinkAdd.c
+++ chapter2/ToLinkAdd.c
@@ -1,10 +1,10 @@
 /**************************************************************************************************
-	> File Name: exam.c
+	> File Name: ToLinkAdd.c
 	> Author: ZhouPan / github:dreamer2018 
 	> Mail: zhoupans_mail@163.com
 	> Blog: blog.csdn.net/it_dream_er
 	> Declared: Signed on behalf of not only belongs , but also represent a responsibility!
-	> Created Time: 2015年11月22日 星期日 14时01分33秒
+	> Created Time: 2015年11月22日 星期日 16时22分33秒
  ************************************************************************************************/
 
 #include<stdio.h>
@@ -52,42 +52,51 @@ SList Created()
 //讲两条升序单链表合成一条
 SList Merge(SList s1,SList s2)
 {
-    SList p,q,r,s,t;
-    p=s1;
-    q=s2;
+    SList p,q,r,s; //定义中间变量
+    
+    p=s1; //p指向第一条链
+    q=s2; //q指向第二条链
+    
+    //如果第一条链上的头小于第二条链上的头，则新的链表头指向第一条
     if(s1->data < s2->data)
     {
         s=s1;
         p=p->next;
     }
-    else
+    else  //如果第二条链上的头小于第一条链上的头，则新的链表的头指向第二条
     {
         s=s2;
         q=q->next;
     }
-    r=s;
+    
+    r=s;  //合成的新链表的头为s
+
     while(p!=NULL && q!=NULL)
     {
+        //如果第一条链上的小于第二条链上的，则下一个结点指向第一个链上较小的结点
         if(p->data < q->data)
         {
             r->next=p;
             p=p->next;
         }
-        else
+        else //如果第二条链上的小于或等于第一条链上的，则下一个结点指向第二个结点上较小的结点
         {
             r->next=q;
             q=q->next;
         }
         r=r->next;
     }
+    //如果第一条链上的还没完，则将后续的加到新链表结尾
     if(p!=NULL)
     {
         r->next=p;
     }
+    //如果第二条链上的还没完,则将后续的加到新链表结尾
     if(q!=NULL)
     {
         r->next=q;
     }
+    //返回新的链表的结点
     return s;
 }
 
