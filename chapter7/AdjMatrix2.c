ce545f63b96275c2a1d8b8fcdc4b70822cb7be87
dreamer2018 <zhoupans_mail@163.com>
2015-11-23 20:21:31 +0800

    add

---
+++ chapter7/AdjMatrix2.c
@@ -0,0 +1,91 @@
+/**************************************************************************************************
+	> File Name: AdjMatrix2.c
+	> Author: ZhouPan / github:dreamer2018 
+	> Mail: zhoupans_mail@163.com
+	> Blog: blog.csdn.net/it_dream_er
+	> Function:无向树的邻阶矩阵表示法
+	> Created Time: 2015年11月23日 星期一 20时06分01秒
+ ************************************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//最大的数据量
+#define MAX 20
+
+//定义正无穷
+#define INFINITY 65535
+
+//数据的类型
+typedef char DataType;
+
+//邻阶矩阵的结构体
+typedef struct
+{
+    //顶点在数组中的位置信息
+    int arcs[MAX][MAX];
+    //顶点储存的信息
+    DataType data[MAX];
+    //顶点个数
+    int vexnum;
+    //边的个数
+    int arcnum;
+    //每个结点权值
+    int weight;
+
+}AdjMatrix;
+
+//临阶矩阵的创建
+void Created(AdjMatrix *a)
+{
+    int i,j,add1,add2;
+    printf("请输入顶点数和边数：");
+    scanf("%d %d",&a->vexnum,&a->arcnum);
+    getchar();
+    for(i=0;i<a->vexnum;i++)
+    {
+        for(j=0;j<a->vexnum;j++)
+        {
+            a->arcs[i][j]=INFINITY;
+        }
+    }
+    printf("请输入%d个顶点:\n",a->vexnum);
+    for(i=0;i<a->vexnum;i++)
+    {
+        printf("No.%d个顶点：",i+1);
+        scanf("%c",&a->data[i]);
+        getchar();
+    }
+    printf("請輸入%d條邊：\n",a->arcnum);
+    for(i=0;i<a->arcnum;i++)
+    {
+        printf("No.%d條邊：",i+1);
+        scanf("%d %d",&add1,&add2);
+        getchar();
+        scanf("%d",&a->weight);
+        a->arcs[add1][add2]=a->weight;
+        a->arcs[add2][add1]=a->weight;
+    }
+}
+
+//打印
+void PrintMatrix(AdjMatrix *a)
+{
+    int i,j;
+    for(i=0;i<a->vexnum;i++)
+    {
+        for(j=0;j<a->vexnum;j++)
+        {
+            printf("%d\t",a->arcs[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    AdjMatrix *a;
+    a=(AdjMatrix *)malloc(sizeof(AdjMatrix));
+    Created(a);
+    printf("\n\n");
+    PrintMatrix(a);
+}
