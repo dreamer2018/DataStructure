d9ae2916fcaac62067390a9ee1ce5099da974dde
dreamer2018 <zhoupans_mail@163.com>
2015-11-25 17:03:02 +0800

    add

--- chapter7/AdjList.c
+++ chapter7/AdjList.c
@@ -3,7 +3,7 @@
 	> Author: ZhouPan / github:dreamer2018 
 	> Mail: zhoupans_mail@163.com
 	> Blog: blog.csdn.net/it_dream_er
-	> Function:邻阶表的创建，图的深度优先，广度优先遍历
+	> Function:无向图的邻阶表的创建，遍历
 	> Created Time: Wed 25 Nov 2015 03:22:55 PM CST
  ************************************************************************************************/
 
@@ -48,46 +48,86 @@ int Located(AdjList *a,DATATYPE d1)
     }
     return -1;
 }
-void Created(AdjList *a)
+
+//将输入的数据插入邻阶表中
+void Insert(AdjList *a,int i,DATATYPE d1,DATATYPE d2)
 {
-    int i,j,k;
-    DATATYPE d1,d2;
+    int j,k;
     ArcNode *p,*q;
-    printf("请输入顶点数和关系数：");
-    scanf("%d %d",a->vexnum,a->arcnum);
-    getchar();
-    printf("请输入%d个顶点：",a->vexnum);
-    for(i=0;i<a->vexnum;i++)
+    j=Located(a,d1);
+    k=Located(a,d2);
+    p=(ArcNode *)malloc(sizeof(ArcNode));
+    p->adj=k+1;
+    p->next=NULL;
+    q=a->vertex[j].head;
+    if(q==NULL)
     {
-        scanf("%c",a->vertex[i].data);
-        getchar();
-        a->vertex[i].head=NULL;
+        a->vertex[j].head=p;
     }
-    printf("请输入%d个关系：",a->arcnum);
-    for(i=0;i<a->arcnum;i++)
+    else if(q->next==NULL)
     {
-        d1=getchar();
-        d2=getchar();
-        j=Located(a,d1);
-        k=Located(a,d2);
-        if(j<0|| k<0)
-        {
-            printf("not this %c",d1);
-            exit(0);
-        }
-        p=(ArcNode *)malloc(sizeof(ArcNode));
-        p->adj=k;
-        p->next=NULL;
-        q=a->vertex[j].head;
-        while(q->next!=NULL)
+        q->next=p;
+    }
+    else
+    {
+        while(q!=NULL && q->next!=NULL)
         {
             q=q->next;
         }
         q->next=p;
     }
 }
+//创建邻阶矩阵
+void Created(AdjList *a)
+{
+    int i,j,k;
+    DATATYPE d1,d2;
+    ArcNode *p,*q;
+    printf("请输入顶点数和关系数：");
+    scanf("%d %d",&a->vexnum,&a->arcnum);
+    getchar();
+    printf("请输入%d个顶点：\n",a->vexnum);
+    for(i=0;i<a->vexnum;i++)
+    {
+        printf("请输入No.%d个顶点：",i+1);
+        scanf("%c",&a->vertex[i].data);
+        getchar();
+        a->vertex[i].head=NULL;
+    }
+    printf("请输入%d个关系：\n",a->arcnum);
+    for(i=0;i<a->arcnum;i++)
+    {
+        printf("请输入No.%d个关系：",i+1);
+        d1=getchar();
+        d2=getchar();
+        getchar();
+        Insert(a,i,d1,d2);
+        Insert(a,i,d2,d1);
+    }
+}
+//打印出邻接矩阵
+void Print(AdjList *a)
+{
+    int i;
+    ArcNode *p,*q;
+    printf("\n\n");
+    for(i=0;i<a->vexnum;i++)
+    {
+        p=a->vertex[i].head;
+        printf("%d : ",i+1);
+        while(p!=NULL)
+        {
+            printf("%d    ",p->adj);
+            p=p->next;
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 int main()
 {
     AdjList *a;
     a=(AdjList *)malloc(sizeof(AdjList));
+    Created(a);
+    Print(a);
 }
